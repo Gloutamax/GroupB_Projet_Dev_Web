77c6c7152f0fcd23c63bfc9290e7cc2d2982b846
Gloutamax <justin.daub.fr@gmail.com>
2025-12-13 00:58:51 +0100

    Ajout code user-service.js

---
+++ frontend/src/services/user-service.js
@@ -0,0 +1,56 @@
+import api from "./api";
+
+export default {
+    getUser: async function () {
+        const response = await api("/user");
+        if (response.status === 401) {
+            alert("You need to be connected");
+            throw new Error("You need to be connected");
+        }
+        return await response.json();
+    },
+    getAllUsers: async function () {
+        const response = await api("/users");
+        if (response.status === 401) {
+            alert("You need to be connected");
+            throw new Error("You need to be connected");
+        }
+        if (response.status === 403) {
+            alert("You don't have permission to view all users");
+            throw new Error("Forbidden");
+        }
+        return await response.json();
+    },
+    createUser: async function (userData) {
+        const response = await api("/users", {
+            method: "POST",
+            body: JSON.stringify(userData),
+        });
+        if (response.status === 401) {
+            alert("You need to be connected");
+            throw new Error("You need to be connected");
+        }
+        return await response.json();
+    },
+    deleteUser: async function (user_id) {
+        const response = await api(`/users/${user_id}`, {
+            method: "DELETE",
+        });
+        if (response.status === 401) {
+            alert("You need to be connected");
+            throw new Error("You need to be connected");
+        }
+        return;
+    },
+    updateUser: async function (user_id, userData) {
+        const response = await api(`/users/${user_id}`, {
+            method: "PATCH",
+            body: JSON.stringify(userData),
+        });
+        if (response.status === 401) {
+            alert("You need to be connected");
+            throw new Error("You need to be connected");
+        }
+        return await response.json(); 
+    }
+}
